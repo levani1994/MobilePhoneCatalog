9712edaefae5a5ca882ab8b79f77336c156d57bc
levani_jalaghonia <levanijalaghonia@gmail.com>
2020-09-09 23:37:26 +0400

    ewewe

---
+++ MobilePhoneCatalog/Models/MobilePhonesDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MobilePhoneCatalog.Models
+{
+    public class MobilePhonesDBContext : DbContext
+    {
+        public DbSet<Publisher> Publishers { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"");
+        }
+    }
+}
---
+++ MobilePhoneCatalog/Models/Publisher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MobilePhoneCatalog.Models
+{
+    public class Publisher
+    {
+        public int PublisherID { get; set; }
+        public string PublisherName { get; set; }
+
+
+    }
+}
